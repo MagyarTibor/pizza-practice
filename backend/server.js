f448c901ae31b9919f45067b79f0a76aa7e27ac5
MagyarTibor <tibormagyar86@gmail.com>
2022-11-13 19:50:05 +0100

    add file

--- backend/server.js
+++ backend/server.js
@@ -5,3 +5,42 @@ const fs = require("fs");
 const app = express();
 
 app.use(express.json()); // IMPORTANT
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(`${__dirname}/../frontend/index.html`));
+});
+
+app.use("/img", express.static(`${__dirname}/data/img`));
+
+app.use("/public", express.static(`${__dirname}/../frontend/public`));
+
+app.get("/pizzas", (req, res) => {
+  fs.readFile(`${__dirname}/data/data.json`, (err, data) => {
+    if (err) {
+      console.log("hiba:", err);
+      res.status(500).send("hibavan");
+    } else {
+      res.status(200).send(JSON.parse(data));
+    }
+  });
+});
+
+app.post("/", (req, res) => {
+  const now = new Date();
+
+  const fileData = JSON.parse(JSON.stringify(req.body));
+
+  const fileDataString = JSON.stringify(fileData, null, 2);
+  const uploadPath = __dirname + "/../backend/data/" + `order`; //`order/${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}-${now.getHours()}-${now.getMinutes()}-${now.getSeconds()}-${now.getMilliseconds()}.json`;
+
+  fs.writeFileSync(uploadPath, fileDataString, (err) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send(err);
+    }
+  });
+
+  return res.send(fileDataString);
+});
+
+app.listen(2222, console.log("server listening on http://127.0.0.1:2222"));
---
+++ frontend/public/script.js
@@ -0,0 +1,112 @@
+const rootElement = document.querySelector("#root");
+
+const pageComponent = () => `<div class="wrapper"></div>`;
+rootElement.insertAdjacentHTML("beforeend", pageComponent());
+
+const pageElement = document.querySelector(".wrapper");
+
+const pizzaComponent = (name, ingredients, price) => `
+  <div class="pizza-card pizzaCard-${name}">
+  <img src="img/${name}.png" alt="">
+  <div class="pizzaInfo">
+  <h2>${name}</h2>
+  <h3>${ingredients}</h3>
+  <h4>${price} HUF</h4>
+  <input type="number" id="${name}" class="pizzaAmount" min="1" />
+  <button class="pizzaCard-${name} order">Rendelés</button>
+  </div>
+  </div>
+`;
+
+const userComponent = () =>
+  `<div class="cart">
+  <div class="orderList">
+  <h2 class="orderTitle">Rendelésem:<h2>
+        <div class="pizzas"></div>
+        </div>
+        <form id="add-data">
+          <input type="text" name="name" id="name" placeholder="Vezetéknév:" />
+          <input type="text" name="surname" id="surname" placeholder="Keresztnév:" />
+          <input type="text" name="zip" id="zip" placeholder="Irányítószám:"  />
+          <input type="text" name="city" id="city" placeholder="Város:" />
+          <input type="text" name="street" id="street" placeholder="Utca:" />
+          <input type="text" name="houseNumber" id="houseNumber" placeholder="Házszám:" />
+          <input type="tel" name="phoneNumber" id="phoneNumber" placeholder="Telefonszám:" />
+          <input type="button" value="Küldés" class="pre-order-button"/>
+          </form>
+          </div>`;
+pageElement.insertAdjacentHTML("beforeend", userComponent());
+
+fetch("/pizzas")
+  .then((res) => res.json())
+  .then((pizzas) => {
+    pizzas.map((pizza) =>
+      pageElement.insertAdjacentHTML(
+        "beforeend",
+        pizzaComponent(pizza.name, pizza.ingredients, pizza.price)
+      )
+    );
+
+    const cardButtons = document.querySelectorAll(".order");
+    cardButtons.forEach((cardButton) => {
+      cardButton.addEventListener("click", function (event) {
+        const className = event.target.classList[0]; //pizzaCard-Hawaii
+        let currentPizza = document.querySelector(`.${className}`);
+
+        let pizzaName = currentPizza.querySelector("h2").innerText;
+        let number = currentPizza.querySelector(`input`).value;
+
+        const cartList = document.querySelector(".pizzas");
+
+        cartList.insertAdjacentHTML(
+          "beforeend",
+          `
+      <div>
+        <h2 class="selected${pizzaName}">${number}db ${pizzaName}</h2>
+      </div>`
+        );
+      });
+    });
+  });
+
+rootElement.insertAdjacentHTML(
+  "beforeend",
+  `
+   <div class="alert confirmation-card">
+      <h2>
+        Köszönjük a rendelést! 
+      </h2>
+      <div class= "finalorderlist">
+      </div>
+      <button class="orderbutton conf">  
+      Megrendel!
+      </button>
+      <button class="cancelbutton conf">
+      Mégse
+      </button>
+  </div>
+  `
+);
+
+let alert = rootElement.querySelector(".alert");
+let orderButton = rootElement.querySelector(".orderbutton");
+let cancelbutton = rootElement.querySelector(".cancelbutton");
+
+const popUpWinConfOrder = rootElement.querySelector(".pre-order-button");
+popUpWinConfOrder.addEventListener("click", function () {
+  alert.classList.add("alertOpen");
+});
+
+orderButton.addEventListener("click", function () {
+  alert.classList.remove("alertOpen");
+
+  let form = rootElement.querySelector("#add-data");
+  form.reset();
+  let order = rootElement.querySelector(".pizzas");
+  order.innerHTML = "";
+  let numReset = rootElement.querySelectorAll(".pizzaAmount");
+  numReset.clearInput();
+});
+cancelbutton.addEventListener("click", function () {
+  alert.classList.remove("alertOpen");
+});
