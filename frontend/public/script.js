9d73e568a18abbda5b1fa8e53e20ea6aa42867be
MagyarTibor <tibormagyar86@gmail.com>
2022-11-20 19:33:22 +0100

    add payFunction

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -1,8 +1,13 @@
 const rootElement = document.querySelector("#root");
 
-rootElement.insertAdjacentHTML("beforeend", `<div class="wrapper"></div>`);
+rootElement.insertAdjacentHTML(
+  "beforeend",
+  `<div class="wrapperleft"></div>
+<div class="wrapperright"></div>`
+);
 
-const pageElement = document.querySelector(".wrapper");
+const pageElementLeft = document.querySelector(".wrapperleft");
+const pageElementRight = document.querySelector(".wrapperright");
 
 let finalOrder = [];
 
@@ -12,19 +17,23 @@ const pizzaComponent = (name, ingredients, price) => `
   <div class="pizzaInfo">
   <h2>${name}</h2>
   <h3>${ingredients}</h3>
-  <h4>${price} HUF</h4>
-  <input type="number" id="${name}" class="pizzaAmount" min="1" />
+  <h4>${price}</h4><h4>HUF</h4>
+  <input type="number" id="${name}" class="pizzaAmount" value="1" min="1">
   <button class="pizzaCard-${name} order">Rendelés</button>
   </div>
   </div>
 `;
-
+//
 const userComponent = () =>
-  `<div class="cart">
-  <div class="orderList">
-  <h2 class="orderTitle">Rendelésem:<h2>
-        <div class="pizzas"></div>
-        </div>
+  `<div class="cart ">
+     <div class="orderList">
+     <h2 class="orderTitle">Rendelésem:<h2>
+     <div class="pizzas"></div>
+  </div>
+  <div class="total">
+  <h2>Total:<h2>
+  <h2 class="total-pay">&nbsp0 HUF</h2>
+  </div>
         <form id="add-data">
           <input type="text" name="name" id="name" placeholder="Név:" />
           <input type="text" name="zip" id="zip" placeholder="Irányítószám:"  />
@@ -35,13 +44,13 @@ const userComponent = () =>
           <input type="button" value="Küldés" class="pre-order-button"/>
           </form>
           </div>`;
-pageElement.insertAdjacentHTML("beforeend", userComponent());
+pageElementRight.insertAdjacentHTML("beforeend", userComponent());
 
 fetch("/pizzas")
   .then((res) => res.json())
   .then((pizzas) => {
-    pizzas.map((pizza) =>
-      pageElement.insertAdjacentHTML(
+    pizzas.forEach((pizza) =>
+      pageElementLeft.insertAdjacentHTML(
         "beforeend",
         pizzaComponent(pizza.name, pizza.ingredients, pizza.price)
       )
@@ -55,17 +64,23 @@ fetch("/pizzas")
 
         let pizzaName = currentPizza.querySelector("h2").innerText;
         let number = currentPizza.querySelector(`input`).value;
+        let price = currentPizza.querySelector(`h4`).innerText;
 
         const cartList = document.querySelector(".pizzas");
-
+        const totalPrice = document.querySelector(".total");
+        let sum = Number(number) * Number(price);
         cartList.insertAdjacentHTML(
           "beforeend",
           `
-      <div class="cart-pizza-${pizzaName}-${number}">
-        <h2 class="selected${pizzaName}">${number}db ${pizzaName}</h2>
-        <button class="delete-pizza-${pizzaName}-${number}">X</button>
-      </div>`
+         <div class=" cartPizza cart-pizza-${pizzaName}-${number}">
+         <h2 class="selected${pizzaName}">${pizzaName} ${number}db&nbsp</h2>
+         <h3 class="one-class-pizza-sum">${sum}</h3>
+         <h3>&nbsp HUF</h3>
+         
+         <button class=" cdpb delete-pizza-${pizzaName}-${number}">X</button>
+         </div>`
         );
+
         //kinyírni a forEachet és az Allt
         const deleteBtnPizza = document.querySelectorAll(
           `.delete-pizza-${pizzaName}-${number}`
@@ -76,8 +91,27 @@ fetch("/pizzas")
               `.cart-pizza-${pizzaName}-${number}`
             );
             deleteFunction.remove();
+
+            totalSumComponent();
           });
         });
+        const totalSumComponent = () => {
+          let arr = [];
+          document
+            .querySelectorAll(".one-class-pizza-sum")
+            .forEach((l) => arr.push(Number(l.innerHTML)));
+
+          let totalArr = arr.reduce((p, c) => p + c, 0);
+
+          const empy = document.querySelector(".total-pay");
+          empy.remove();
+
+          totalPrice.insertAdjacentHTML(
+            "beforeend",
+            `<h2 class="total-pay">&nbsp${totalArr}&nbspHUF</h2>`
+          );
+        };
+        totalSumComponent();
       });
     });
   });
@@ -86,10 +120,14 @@ rootElement.insertAdjacentHTML(
   "beforeend",
   `
    <div class="alert confirmation-card">
+   <div class="order-info">
+   
+   </div>
       <h2>
         Köszönjük a rendelést! 
       </h2>
       <div class= "finalorderlist">
+      
       </div>
       <button class="orderbutton conf">  
       Megrendel!
@@ -106,11 +144,11 @@ let orderButton = rootElement.querySelector(".orderbutton");
 let cancelbutton = rootElement.querySelector(".cancelbutton");
 
 const popUpWinConfOrder = rootElement.querySelector(".pre-order-button");
-popUpWinConfOrder.addEventListener("click", function () {
+popUpWinConfOrder.addEventListener("click", () => {
   alert.classList.add("alertOpen");
 });
 
-orderButton.addEventListener("click", function () {
+orderButton.addEventListener("click", () => {
   alert.classList.remove("alertOpen");
 
   let order = document.querySelectorAll(".pizzas>div>h2");
@@ -127,6 +165,7 @@ orderButton.addEventListener("click", function () {
     houseNumber: document.querySelector("#houseNumber").value,
     phoneNumber: document.querySelector("#phoneNumber").value,
     order: finalOrder,
+    price: document.querySelector(".total-pay").innerText,
     date: new Date(),
   };
   fetch(`/`, {
